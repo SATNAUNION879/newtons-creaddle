948a8905504ed30ac070d2d6f562e2aa420c16f6
SATNAUNION879 <75133113+SATNAUNION879@users.noreply.github.com>
2020-12-05 14:39:56 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# newtons-creaddle
+# MatterJSBoilerPlate
+MatterJSBoilerPlate
---
+++ bob.js
@@ -0,0 +1,26 @@
+class Bob {
+    constructor(x,y,radius) {
+        var opt = {
+            'resitution':1,
+            'isStatic': false,
+            'density': 0.5
+        }
+        this.x = x;
+        this.y = y;
+        this.body = Bodies.circle(x,y,radius,opt);
+
+        World.add(world,this.body);
+    }
+
+    display() {
+        var pos = this.body.position;
+        var angle = this.body.angle;
+        push();
+        translate(pos.x,pos.y);
+        rotate(angle);
+        ellipseMode(RADIUS);
+        fill("lightblue");
+        ellipse(0,0,this.body.circleRadius,this.body.circleRadius);
+        pop();
+    }
+}
